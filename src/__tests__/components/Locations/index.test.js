25d9a61fe4edb28e82514bbc5c6fb76ef9dcb857
Ali Zand <ali.zand@realpage.com>
2020-05-01 23:38:25 -0700

    Fixed it

--- src/__tests__/components/Locations/index.test.js
+++ src/__tests__/components/Locations/index.test.js
@@ -8,11 +8,11 @@ import Location from '../../../components/Locations/Location';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Locations', () => {
-  const wrapper = shallow(<Locations />)
+  const wrapper = shallow(<Locations />);
 
   describe('has locations', () => {
     it('shows as many locations as there are ', () => {
       expect(wrapper.find(Location)).toHaveLength(4);
-    })
+    });
   });
 });

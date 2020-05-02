0d3c4ab8640dca216a1538debb94a9f64019c0a6
Ali Zand <ali.zand@realpage.com>
2020-05-01 23:20:24 -0700

    Fix Travis?

---
+++ src/__tests__/components/Locations/index.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import Locations from '../../../components/Locations';
+import Location from '../../../components/Locations/Location';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Locations', () => {
+  const wrapper = shallow(<Locations />)
+
+  describe('has locations', () => {
+    it('shows as many locations as there are ', () => {
+      expect(wrapper.find(Location)).toHaveLength(4);
+    })
+  });
+});
